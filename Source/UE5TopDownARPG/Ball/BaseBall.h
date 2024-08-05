c5ae37d0d8d3e88bbcc8315188ca9c69cb0d5114
swordpepi <peter.hristov03@abv.bg>
2024-08-05 23:32:12 +0300

    added Ball

---
+++ Source/UE5TopDownARPG/Ball/BaseBall.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseBall.generated.h"
+
+UCLASS()
+class UE5TOPDOWNARPG_API ABaseBall : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseBall();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	UFUNCTION()
+	void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	UPROPERTY(EditDefaultsOnly)
+	class USphereComponent* SphereComponent;
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
