f2d109397e88c49e20a2d02580399df9bad6ad4d
Alexander <alexander.terezov@gmail.com>
2024-08-08 00:26:08 +0300

    Fixed ball bug

--- Source/UE5TopDownARPG/Ball/BaseBall.cpp
+++ Source/UE5TopDownARPG/Ball/BaseBall.cpp
@@ -1,28 +1,24 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "BaseBall.h"
 #include "Kismet/GameplayStatics.h"
 #include "Components/SphereComponent.h"
 #include "../UE5TopDownARPG.h"
 
-
 // Sets default values
 ABaseBall::ABaseBall()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("CollisionSphereComponent"));
 	SphereComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
 	SphereComponent->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
-	RootComponent = SphereComponent;
-
-	//MeshComponent = Cast<UStaticMeshComponent>(SphereComponent->GetChildActor;
-	//MeshComponent->RegisterComponent();
 
 	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BallMesh"));
-	MeshComponent->AttachToComponent(SphereComponent, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
+	RootComponent = MeshComponent;
+
+	SphereComponent->SetupAttachment(MeshComponent);
 
 	SphereComponent->OnComponentBeginOverlap.AddUniqueDynamic(this, &ABaseBall::OnBeginOverlap);
 	SphereComponent->OnComponentEndOverlap.AddUniqueDynamic(this, &ABaseBall::OnEndOverlap);
@@ -31,7 +27,7 @@ ABaseBall::ABaseBall()
 // Called when the game starts or when spawned
 void ABaseBall::BeginPlay()
 {
-	Super::BeginPlay(); 
+	Super::BeginPlay();
 }
 
 void ABaseBall::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
